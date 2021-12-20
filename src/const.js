d1ff5845341b6ee59e51e0ca7f933df69b0ef320
yuan30 <yuan30.x@gmail.com>
2021-12-20 16:54:16 +0800

    rpc

--- src/const.js
+++ src/const.js
@@ -17,18 +17,18 @@ export const multicallConfig = {
       url: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
       address: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
     },
-    // [ChainId.BSC]: {
-    //   url: 'https://bsc-dataseed.binance.org/',
-    //   address: '',
-    // },
-    // [ChainId.HECO]: {
-    //   url: 'https://http-mainnet-node.huobichain.com',
-    //   address: '',
-    // },
-    // [ChainId.MATIC]: {
-    //   url: 'https://polygon-rpc.com/',
-    //   address: '',
-    // }
+    [ChainId.BSC]: {
+      url: 'https://bsc-dataseed.binance.org/',
+      address: '0x6427169aB7344F9C37E9dC9001c681BEcd09343d',
+    },
+    [ChainId.HECO]: {
+      url: 'https://http-mainnet-node.huobichain.com',
+      address: '0xBF4b1bE1F00F5624ba4D65f8548ccF6E75d0deFe',
+    },
+    [ChainId.MATIC]: {
+      url: 'https://polygon-rpc.com/',
+      address: '0x6427169aB7344F9C37E9dC9001c681BEcd09343d',
+    }
   }
 }
 
