df51bbc2175df4a6937181c0fc2754f32edcba76
yuan30 <yuan30.x@gmail.com>
2022-04-07 15:25:23 +0800

    d.ts

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+
+export function multicallClient(call: Array<Function>): Promise<any[]>;
+
+interface Rpc {
+  url: string,
+  address: string
+}
+
+export declare enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  BSC = 56,
+  ETM3 = 36,
+  ETM3Test = 37,
+  ETH = 1,
+  HECO = 128,
+  MATIC = 137
+}
+interface rpcMap {
+  [key: string]: Rpc
+}
+
+
+export interface Config {
+  defaultChainId: number,
+  delay: number,
+  timeout: number,
+  maxCalls: number,
+  allowFailure: boolean,
+  rpc: rpcMap
+}
+
+export function config(configData: Config): Config;
+
+export class Contract{
+  constructor(abi: Array<Object>, address: string, chainId?: number)
+}
